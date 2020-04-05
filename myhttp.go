186767fa14c0a878d593b49e465b1687c0484f0a
RazaChohan <razachohan@gmail.com>
2020-04-05 23:26:57 +0200

    add error handling + scheme addition to url

--- myhttp.go
+++ myhttp.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -17,23 +18,41 @@ func main() {
 	urls := flag.Args()
 	fmt.Println(*threads)
 	//Loop on urls passed
-	for _, url := range urls {
-		responseBody := SendHttpRequest(url)
-		fmt.Println(url + " " + GetMd5Hash(responseBody))
+	for _, urlString := range urls {
+		urlString := AddSchemeToUrl(urlString)
+		responseBody, err  := SendHttpRequest(urlString)
+		var outputToPrint string
+		if err == nil {
+			outputToPrint = urlString + " << RESPONSE HASH >> " + GetMd5Hash(responseBody)
+		} else {
+			outputToPrint = urlString + " << RESPONSE ERROR MSG >> " + err.Error()
+		}
+		fmt.Println(outputToPrint)
 	}
 }
 //Send http request
-func SendHttpRequest(url string) string {
+func SendHttpRequest(url string) (string, error) {
 	response, err := http.Get(url)
-	bodyBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
+	var responseStr string
+	if err == nil && response.StatusCode == 200 {
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		responseStr = string(bodyBytes)
 	}
-	return string(bodyBytes)
+	return responseStr, err
 }
 //Get MD5 hash
 func GetMd5Hash(responseBody string) string {
 	hash := md5.Sum([]byte(responseBody))
 	return hex.EncodeToString(hash[:])
-
+}
+//Add scheme to url
+func AddSchemeToUrl(urlStr string) string {
+	parsedUrl, _ := url.Parse(urlStr)
+	if len(parsedUrl.Scheme) == 0 {
+		urlStr = "http://" + urlStr
+ 	}
+	return urlStr
 }
